9338a73addb143ed03475cd05c442710eaa63ef3
Dmitry Frank <dimon.frank@gmail.com>
2015-07-28 21:00:00 +0300

    added todo

--- src/gui/handler_view/handler_view.cpp
+++ src/gui/handler_view/handler_view.cpp
@@ -55,19 +55,28 @@ QWidget *HandlerView::createWidget() const
 {
    QWidget *p_widg = new QWidget();
 
-   QBoxLayout *p_lay = new QBoxLayout(QBoxLayout::TopToBottom);
+   QBoxLayout *p_vert_lay = new QBoxLayout(QBoxLayout::TopToBottom);
 
    {
       QLabel *p_label = new QLabel("Handler: " + p_handler->getName());
-      p_lay->addWidget(p_label);
+      p_vert_lay->addWidget(p_label);
    }
 
+   //TODO: make button like "edit", and when it's pressed, new dock widget
+   //      should be created.
+   //
+   //      Need to think how to account these dock widgets: the GUI
+   //      should be prepared to handle event when scripts are changed
+   //      from outside (although it is not going to happen, but anyway)
+#if 0
    {
       QPlainTextEdit *p_code = new QPlainTextEdit(p_handler->getScript());
-      p_lay->addWidget(p_code);
+      p_vert_lay->addWidget(p_code);
+      p_code->setHidden(true);
    }
+#endif
 
-   p_widg->setLayout(p_lay);
+   p_widg->setLayout(p_vert_lay);
    
    return p_widg;
 }
